401362cbeaa88052a3377930481591823fd2c248
ralphdynasty <ralphdynasty@gmail.com>
2020-08-31 23:47:29 +0100

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,8 +12,9 @@ const forecast = (lat, long, callback) => {
             const current = body.current
             const temperature = current.temperature
             const feelLike = current.feelslike
+            const humidity = current.humidity
 
-            callback(undefined, current.weather_descriptions[0] + '. It is currently ' + temperature + ' degrees but it feels like its ' + feelLike + " degrees")
+            callback(undefined, current.weather_descriptions[0] + '. It is currently ' + temperature + ' degrees but it feels like its ' + feelLike + " degrees. The humidity is " + humidity)
         }
     })
 }
